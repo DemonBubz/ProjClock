715cdfa34ca283cbfd3515263cbb81500f73efc5
Apophis_ <ancientphoebus@gmail.com>
2021-06-10 19:46:29 +0530

    minor change

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,7 +1,7 @@
 function beginConstruct(){
     if(!clock12Running && !clock24Running){
         timeZoneMenu(importedTimeZoneList)
-         x1=setInterval(function(){showClock12(defaultZone,"firstCunt")},1000);
+         x1=setInterval(function(){showClock12(defaultZone,"zeroCunt")},1000);
         }
         dropDown1Interface();
         dropDown2Interface();
