50959188f00fef863bd95b1f6d9c394a9c3a6a1b
Apophis_ <ancientphoebus@gmail.com>
2021-06-11 16:21:57 +0530

    added design for variable containers

--- JavaScript/card.js
+++ JavaScript/card.js
@@ -1,15 +1,22 @@
 let x1, x2, selectedZoneC1 = defaultZone, selectedZoneC2 = defaultZone;
+
 var firstCardRunning=false;
 function dropDown1Interface() {
     var existingElem=document.querySelector('#zeroCunt');
     var cardElem=existingElem.cloneNode(true);
     cardElem.class='container';
     cardElem.id='firstCunt';
-    cardElem.style.width="26vw";
+    cardElem.style.boxSizing="border-box";
+    cardElem.style.width="33vw";
     cardElem.style.display="inline-block";
     cardElem.style.position="relative";
-    cardElem.style.fontSize="26px";
-    cardElem.style.right="2px";
+    cardElem.style.fontSize="2vw";
+    // cardElem.style.right="2px"
+    cardElem.style.float="2px";
+    // cardElem.style.background="linear-gradient(to right, rgb(122 123 52), rgb(154 143 9))";
+    // cardElem.style.background="linear-gradient(to right, rgb(13 10 27), rgb(13 25 47))";
+
+   
     
     select[0].addEventListener("click", x => {
        existingElem.after(cardElem);
@@ -35,11 +42,17 @@ function dropDown2Interface() {
     var cardElem = existingElem.cloneNode(true);
     cardElem.class = 'container';
     cardElem.id = 'secondCunt';
-    cardElem.style.width="26vw";
+    cardElem.style.boxSizing="border-box";
+    cardElem.style.width="33vw";
     cardElem.style.display="inline-block";
-    cardElem.style.fontSize="26px";
+    cardElem.style.fontSize="2vw";
     cardElem.style.position="realtive";
-    cardElem.style.right="2px";
+    cardElem.style.clear="both";
+    cardElem.style.float="right";
+    // cardElem.style.right="2px"
+    // cardElem.style.background="linear-gradient(to right, rgb(109 18 11), rgb(26, 1, 1))";
+    // cardElem.style.background="linear-gradient(to right, rgb(13 10 27), rgb(13 25 47))";
+    
     
     // cardElem.style.float="right";
     select[1].addEventListener("click", function () {
